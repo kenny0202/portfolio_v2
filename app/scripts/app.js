5e8b37ead81414eaeaa0baa93fd6b9d01ea0c881
Kenny <kenny020259@hotmail.com>
2016-05-22 12:42:17 -0700

    Added Tools Developer

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -72,16 +72,3 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   };
 
 })(document);
-
-document.getElementById('formPost').addEventListener('iron-form-submit', display);
-function display(event) {
-      var output = document.getElementById('output');
-      output.innerHTML = JSON.stringify(event.detail);
-    }
-
-    function submitHandler(event) {
-      Polymer.dom(event).localTarget.parentElement.submit();
-    }
-    function resetHandler(event) {
-      Polymer.dom(event).localTarget.parentElement.reset();
-    }
